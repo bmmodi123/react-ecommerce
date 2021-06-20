23cdc0fec92daf41f22de5b1d921329c490ca4e6
Chitrang Modi <11bce053@nirmauni.ac.in>
2021-06-21 01:18:45 +0530

    section 3 video 71

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import HomePage from './homepage.component.jsx';
+
 import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
