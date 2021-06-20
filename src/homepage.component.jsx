28c1730b73564b201db13a536d59b9fa19232eb4
Chitrang Modi <11bce053@nirmauni.ac.in>
2021-06-20 20:01:39 +0530

    Upadate on 20-6-2021

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -4,6 +4,7 @@ import './homepage.styles.scss';
 const HomePage = () => (
     <div className='homepage'>
         <div className='directory-menu'>
+        
             <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>HATS</h1>
